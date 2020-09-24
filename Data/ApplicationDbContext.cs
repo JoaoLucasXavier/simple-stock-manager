fe80ce1b2cd02a9c6a5ce858c09d029207494d23
JoaoLucasXavier <lucasxavier000@gmail.com>
2020-09-24 16:28:14 -0300

    Set db context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using simple_stock_manager.Models;
 
 namespace simple_stock_manager.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<CustomerAddress> CustomersAddress { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<SupplierAddress> SuppliersAddress { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Shopping> Shopping { get; set; }
     }
 }
