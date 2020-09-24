4ee38d51b9b6bb0d7224d50d7c42b253a45a8d59
JoaoLucasXavier <lucasxavier000@gmail.com>
2020-09-24 17:52:24 -0300

    Settings the context and query string

--- Startup.cs
+++ Startup.cs
@@ -28,8 +28,8 @@ namespace simple_stock_manager
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
