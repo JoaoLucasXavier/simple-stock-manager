c21caad82f8ee4212d3262124e247364f3cd94b1
JoaoLucasXavier <lucasxavier000@gmail.com>
2020-09-25 15:49:49 -0300

    Cleaning the controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,29 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using simple_stock_manager.Models;
 
 namespace simple_stock_manager.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
