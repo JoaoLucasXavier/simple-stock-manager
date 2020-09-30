1d86edee8899c395a87bfd9cf9c658542361021e
JoaoLucasXavier <lucasxavier000@gmail.com>
2020-09-30 01:11:43 -0300

    Add dusplay name

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -15,6 +15,7 @@ namespace simple_stock_manager.Models
         [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
         public string Document { get; set; }
 
+        [DisplayName("Tipo")]
         public TypeOfPerson CustomerType { get; set; }
 
         public CustomerAddress Address { get; set; }
